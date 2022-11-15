39840644ee304ebb60d6d23fc0619c30060ea4fb
HyungJin Han <han1210_36@naver.com>
2022-11-15 21:26:22 +0900

    update, delete 진행 중

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ import fastapi as _fastapi
 import fastapi.security as _security
 import sqlalchemy.orm as _orm
 import services as _services, schemas as _schemas
+from typing import List
 
 app = _fastapi.FastAPI()
 
@@ -38,4 +39,38 @@ async def create_lead(
   user: _schemas.User=_fastapi.Depends(_services.get_current_user),
   db: _orm.Session=_fastapi.Depends(_services.get_db)
 ):
-  pass
+  return await _services.create_lead(user=user, db=db, lead=lead)
+
+@app.get('/api/leads', response_model=List[_schemas.Lead])
+async def get_leads(
+  user: _schemas.User=_fastapi.Depends(_services.get_current_user),
+  db: _orm.Session=_fastapi.Depends(_services.get_db)
+):
+  return await _services.get_leads(user=user, db=db)
+
+@app.get('/api/leads/{lead_id}', status_code=200)
+async def get_lead(
+  lead_id: int,
+  user: _schemas.User=_fastapi.Depends(_services.get_current_user),
+  db: _orm.Session=_fastapi.Depends(_services.get_db)
+):
+  return await _services.get_lead(lead_id, user, db)
+
+@app.delete('/api/leads/{lead_id}', status_code=204)
+async def delete_lead(
+  lead_id: int,
+  user: _schemas.User=_fastapi.Depends(_services.get_current_user),
+  db: _orm.Session=_fastapi.Depends(_services.get_db)
+):
+  await _services.delete_lead(lead_id, user, db)
+  return {'message', 'Successfully Deleted'}
+
+@app.put('/api/leads/{lead_id}', status_code=200)
+async def update_lead(
+  lead_id: int,
+  lead: _schemas.LeadCreate,
+  user: _schemas.User=_fastapi.Depends(_services.get_current_user),
+  db: _orm.Session=_fastapi.Depends(_services.get_db)
+):
+  await _services.update_lead(lead_id, lead, user, db)
+  return {'message', 'Successfully Updated'}
